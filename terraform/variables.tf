880b253499f9bc755955cdbb4ef01d4f00c1fdf3
Michael Ullrich <michael.ullrich@acai.gmbh>
2025-11-03 14:46:37 +0100

    add license headers

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,14 @@
+# ACAI Solutions
+# Copyright (C) 2025 ACAI GmbH
+#
+# This file is part of ACAI VECTO. Visit https://www.acai.gmbh or https://docs.acai.gmbh for more information.
+# 
+# Proprietary and Confidential - Licensed under ACAI Rahmen-Lizenzvertrag + Order Form (Subscription required)
+# For full license text, see LICENSE file in repository root.
+#
+# For commercial licensing, contact: contact@acai.gmbh
+
+
 variable "vecto_setup_settings" {
   description = "Info to setup the VECTO CI/CD resources."
   type = object({
